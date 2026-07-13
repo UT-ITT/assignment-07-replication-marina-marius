4cbd6235f3eb0795c297d453adca9221057f30a3
m-banti <marina.banti@student.uni-tuebingen.de>
2026-07-13 23:04:45 +0200

    add: dungeon hud overlay

---
+++ implementation/hud.py
@@ -0,0 +1,8 @@
+# this file is the for the dungeon overlay
+# so the idea is following:
+# P1 can do the following with theri voice
+# 1. change color of shield
+# 2. shange size of shield
+# 3. change duration of shield
+# to set those P2 need to pinch the right overlay so P1 can start using all the vocal chords they have
+# why? because it's easier handling for us (in my opinion)
