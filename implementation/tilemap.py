4d58e779cb98a4083c5f460707296ebc9503cba3
m-banti <marina.banti@student.uni-tuebingen.de>
2026-07-13 23:14:26 +0200

    add: tilemap file

---
+++ implementation/tilemap.py
@@ -0,0 +1,5 @@
+# here comes the tilemap/sprite logic
+# why an own file? since we have 3 different maps
+# overworld, dungeon, treasure chamber
+# so here everything is set and the corresponding state screen file can get the tilemap batch they need
+# keyowrds will be: overworld, dungeon and treasure
