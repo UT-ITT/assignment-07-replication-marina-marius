e2a8653b517411f0109f5e1d77e01484a945307c
m-banti <marina.banti@student.uni-tuebingen.de>
2026-07-13 23:09:19 +0200

    add: interactible file

---
+++ implementation/interactable.py
@@ -0,0 +1,3 @@
+# here comes all logic for various interactible objects (dont know actually if we need it seperately but would be i think easier to debug and look for stuff)
+# also normaly you would do everything seperately e.g. unity so why not here also hihi
+# actually you would do cool objects :( with own scripts that can inherent from a mother class)
