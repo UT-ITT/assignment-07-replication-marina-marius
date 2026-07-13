be641279fd30a54609fa3af603b9fcae9720d2be
m-banti <marina.banti@student.uni-tuebingen.de>
2026-07-14 01:40:52 +0200

    update: tileset resizing

--- implementation/config.py
+++ implementation/config.py
@@ -1,5 +1,13 @@
-# we can throw here all basic constant configs we need for all files that get used in multiple ones 
+# we can throw here all basic constant configs we need for all files that get used in multiple ones
 # e.g gesture recognizer and audio input could/shoudl/would be added here
+import pyglet
+from pyglet.gl import GL_NEAREST
+
+# pixel art tiles are small (e.g. 16x16) and get scaled up
+# without they would look blurry (pyglet/OpenGL default to linear filtering)
+pyglet.image.Texture.default_min_filter = GL_NEAREST
+pyglet.image.Texture.default_mag_filter = GL_NEAREST
+
 WIN_WIDTH = 1280
 WIN_HEIGHT = 720
 WIN_TITLE = "Insert super duper cool title"
