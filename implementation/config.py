4bd5c7922a9a302b153543cb145dd0f19d8c697c
m-banti <marina.banti@student.uni-tuebingen.de>
2026-07-13 23:06:25 +0200

    add: basic configs

---
+++ implementation/config.py
@@ -0,0 +1,14 @@
+# we can throw here all basic constant configs we need for all files that get used in multiple ones 
+# e.g gesture recognizer and audio input could/shoudl/would be added here
+WIN_WIDTH = 1280
+WIN_HEIGHT = 720
+WIN_TITLE = "Insert super duper cool title"
+FPS = 60
+
+TILE_SIZE = 64
+
+TEXT_COLOR = (240, 240, 240, 255)
+ACCENT_COLOR = (255, 200, 60, 255)
+
+P1_COLOR = (220, 60, 60, 255) # singer, keyboard WASD
+P2_COLOR = (60, 140, 220, 255) # support, gesture / arrow keys
